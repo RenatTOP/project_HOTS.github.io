a79e05499d12a726b6e0c71ba70a6446ddbb616e
RenatTOP <renatseverski@gmail.com>
2020-05-11 18:00:49 +0300

    add LiveInternet

--- en-us/additem_en.js
+++ en-us/additem_en.js
@@ -26,7 +26,14 @@ jQuery.ajax({
                         class="sr-only">(current)</span></a>
             </li>
         </ul>
-    </div>`);
+    </div>
+    <!--LiveInternet counter--><script>
+new Image().src = "//counter.yadro.ru/hit?r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";h"+escape(document.title.substring(0,150))+
+";"+Math.random();</script><!--/LiveInternet-->`);
     }
     });
 
--- ru/additem.js
+++ ru/additem.js
@@ -25,7 +25,14 @@ jQuery.ajax({
                 <a class="nav-link" href="/ru/heroes/heroes.html">Герои <span class="sr-only">(current)</span></a>
             </li>
         </ul>
-    </div>`);
+    </div>
+    <!--LiveInternet counter--><script>
+new Image().src = "//counter.yadro.ru/hit?r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";h"+escape(document.title.substring(0,150))+
+";"+Math.random();</script><!--/LiveInternet-->`);
     }
     });
 
