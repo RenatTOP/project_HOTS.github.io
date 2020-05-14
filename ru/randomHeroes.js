2cd45bc2744239df3399622a748d914c68ddb0c8
RenatTOP <renatseverski@gmail.com>
2020-05-14 19:58:48 +0300

    fix

--- ru/randomHeroes.js
+++ ru/randomHeroes.js
@@ -7,7 +7,7 @@ heroes[0] = `<div class="rdmHeroes col-3">
     </a>
 </div>`;
 heroes[1] = `<div class="rdmHeroes col-3">
-    <a href="/ru/heroes/tank/anubarak.html"><img src="/image/heroes/Anubarak/Anubarakbgm.jpg" alt="" class="rdmImg">
+    <a href="/ru/heroes/tank/anubarak.html"><img src="/image/heroes/Anubarak/anubarakbgm.jpg" alt="" class="rdmImg">
         <h3>Ануб'арак</h3>
     </a>
 </div>`;
