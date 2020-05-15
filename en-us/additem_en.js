a6cd6aa49a57ea63abee2d862492f7d34e5e45d3
RenatTOP <renatseverski@gmail.com>
2020-05-15 20:18:04 +0300

    big update 2

--- en-us/additem_en.js
+++ en-us/additem_en.js
@@ -1,32 +1,9 @@
 // Navbar
 jQuery.ajax({
+    url: "/en-us/navbar.html",
+    dataType: "html",
     success: function(response) {
-    document.getElementById('navbar').insertAdjacentHTML("afterbegin", `<a class="navbar-brand" href="/en-us/home.html"><img src="/image/home/logo.png" alt="" id="logo"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Game
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="/en-us/game/roles.html">Roles</a>
-                    <a class="dropdown-item" href="/en-us/game/game-modes.html">Modes</a>
-                    <a class="dropdown-item" href="/en-us/game/brawl.html">Brawl</a>
-                    <a class="dropdown-item" href="/en-us/game/battlefield.html">Battlegrounds</a>
-                    <a class="dropdown-item" href="/en-us/game/rating_matches.html">Ranked: Storm League</a>
-                </div>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="/en-us/heroes/heroes.html">Heroes<span class="sr-only">(current)</span></a>
-            </li>
-        </ul>
-    </div>`);
+    document.getElementById('navbar').innerHTML = response;
     }
     });
 
--- ru/additem.js
+++ ru/additem.js
@@ -1,35 +1,11 @@
 // Navbar
 jQuery.ajax({
+    url: "/ru/navbar.html",
+    dataType: "html",
     success: function(response) {
-    document.getElementById('navbar').insertAdjacentHTML("afterbegin", `<a class="navbar-brand" href="/ru/home.html"><img src="/image/home/logo.png" alt="" id="logo"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Игра
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="/ru/game/roles.html">Роли</a>
-                    <a class="dropdown-item" href="/ru/game/game-modes.html">Режимы</a>
-                    <a class="dropdown-item" href="/ru/game/brawl.html">Потасовка</a>
-                    <a class="dropdown-item" href="/ru/game/battlefield.html">Поля боя</a>
-                    <a class="dropdown-item" href="/ru/game/rating_matches.html">Рейтинговые матчи</a>
-                </div>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="/ru/heroes/heroes.html">Герои<span class="sr-only">(current)</span></a>
-            </li>
-        </ul>
-    </div>`);
+    document.getElementById('navbar').innerHTML = response;
     }
     });
-
 //Footer
 jQuery.ajax({
     url: "/ru/footer.html",
