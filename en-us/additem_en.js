a67765e2e1a30d37e7c9bf898929027914d242e0
RenatTOP <renatseverski@gmail.com>
2020-05-30 17:28:11 +0300

    add preloader

--- en-us/additem_en.js
+++ en-us/additem_en.js
@@ -1,33 +1,35 @@
+//Preloader
+
+window.onload = function () {
+  document.body.classList.add("loaded_hiding");
+  window.setTimeout(function () {
+    document.body.classList.add("loaded");
+    document.body.classList.remove("loaded_hiding");
+  }, 500);
+};
+
 // Navbar
+
 jQuery.ajax({
-    url: "/en-us/navbar.html",
-    dataType: "html",
-    success: function(response) {
-    document.getElementById('navbar').innerHTML = response;
-    }
-    });
+  url: "/en-us/navbar.html",
+  dataType: "html",
+  success: function (response) {
+    document.getElementById("navbar").innerHTML = response;
+  },
+});
 
 //Footer
+
 jQuery.ajax({
-    url: "/en-us/footer.html",
-    dataType: "html",
-    success: function(response) {
-    document.getElementById('footer').innerHTML = response;
-    }
-    });
-
-//Stats
-document.getElementById('dmg').textContent = "Damage";
-document.getElementById('spt').textContent = "Utility";
-document.getElementById('hp').textContent = "Survivability";
-document.getElementById('dfct').textContent = "Complexity";
-
-//Abilities
-document.getElementById('heroic').textContent = "Heroic Abilities";
-document.getElementById('basic').textContent = "Primary Abilities";
-document.getElementById('trait').textContent = "Trait";
+  url: "/en-us/footer.html",
+  dataType: "html",
+  success: function (response) {
+    document.getElementById("footer").innerHTML = response;
+  },
+});
 
 //AOS Effect
+
 AOS.init();
 
 AOS.init({
@@ -36,3 +38,16 @@ AOS.init({
   once: false,
   mirror: false,
 });
+
+//Stats
+
+document.getElementById("dmg").textContent = "Damage";
+document.getElementById("spt").textContent = "Utility";
+document.getElementById("hp").textContent = "Survivability";
+document.getElementById("dfct").textContent = "Complexity";
+
+//Abilities
+
+document.getElementById("heroic").textContent = "Heroic Abilities";
+document.getElementById("basic").textContent = "Primary Abilities";
+document.getElementById("trait").textContent = "Trait";
--- ru/additem.js
+++ ru/additem.js
@@ -1,33 +1,35 @@
+//Preloader
+
+window.onload = function () {
+  document.body.classList.add("loaded_hiding");
+  window.setTimeout(function () {
+    document.body.classList.add("loaded");
+    document.body.classList.remove("loaded_hiding");
+  }, 500);
+};
+
 // Navbar
+
 jQuery.ajax({
-    url: "/ru/navbar.html",
-    dataType: "html",
-    success: function(response) {
-    document.getElementById('navbar').innerHTML = response;
-    }
-    });
-    
+  url: "/ru/navbar.html",
+  dataType: "html",
+  success: function (response) {
+    document.getElementById("navbar").innerHTML = response;
+  },
+});
+
 //Footer
+
 jQuery.ajax({
-    url: "/ru/footer.html",
-    dataType: "html",
-    success: function(response) {
-    document.getElementById('footer').innerHTML = response;
-    }
-    });
-
-//Stats
-document.getElementById('dmg').textContent = "Урон";
-document.getElementById('spt').textContent = "Поддержка";
-document.getElementById('hp').textContent = "Живучесть";
-document.getElementById('dfct').textContent = "Сложность";
-
-//Abilities
-document.getElementById('heroic').textContent = "Героические";
-document.getElementById('basic').textContent = "Базовые";
-document.getElementById('trait').textContent = "Умение";
+  url: "/ru/footer.html",
+  dataType: "html",
+  success: function (response) {
+    document.getElementById("footer").innerHTML = response;
+  },
+});
 
 //AOS Еффекты
+
 AOS.init();
 
 AOS.init({
@@ -36,3 +38,16 @@ AOS.init({
   once: false,
   mirror: false,
 });
+
+//Stats
+
+document.getElementById("dmg").textContent = "Урон";
+document.getElementById("spt").textContent = "Поддержка";
+document.getElementById("hp").textContent = "Живучесть";
+document.getElementById("dfct").textContent = "Сложность";
+
+//Abilities
+
+document.getElementById("heroic").textContent = "Героические";
+document.getElementById("basic").textContent = "Базовые";
+document.getElementById("trait").textContent = "Умение";
